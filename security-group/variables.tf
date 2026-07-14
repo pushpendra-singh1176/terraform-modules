b362682ddc359572b014d130de0cae96fe729d84
Pushpendra Singh <pushpendrasingh0549@gmail.com>
2026-07-14 12:48:15 +0530

    Update variables.tf

--- security-group/variables.tf
+++ security-group/variables.tf
@@ -1,21 +1,34 @@
 variable "name" {
   description = "Security Group Name"
   type        = string
+  validation {
+  condition     = length(trimspace(var.name)) > 0
+  error_message = "Security Group Name cannot be empty."
+}
 }
 
 variable "description" {
   description = "Security Group Description"
   type        = string
+  validation {
+  condition     = length(trimspace(var.description)) > 0
+  error_message = "Security Group Description cannot be empty."
+}
 }
 
 variable "vpc_id" {
   description = "VPC ID"
   type        = string
+  validation {
+  condition     = length(trimspace(var.vpc_id)) > 0
+  error_message = "VPC ID cannot be empty."
+}
 }
 
 variable "tags" {
   description = "Tags"
   type        = map(string)
+  default     = {}
 }
 
 variable "ingress_rules" {
@@ -28,6 +41,7 @@ variable "ingress_rules" {
     protocol    = string
     cidr_blocks = list(string)
   }))
+  default = []
 }
 
 variable "egress_rules" {
@@ -40,4 +54,5 @@ variable "egress_rules" {
     protocol    = string
     cidr_blocks = list(string)
   }))
+  default = []
 }
